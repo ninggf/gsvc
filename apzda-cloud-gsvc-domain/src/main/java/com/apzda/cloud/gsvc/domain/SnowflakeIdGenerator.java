dba837e3064e98fc69b723e44fa7b0371a8ea8f6
ninggf <windywany@163.com>
2023-12-31 15:36:21 +0800

    snowflake id generator

---
+++ apzda-cloud-gsvc-domain/src/main/java/com/apzda/cloud/gsvc/domain/SnowflakeIdGenerator.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023-2023 Fengz Ning (windywany@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.apzda.cloud.gsvc.domain;
+
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.net.NetUtil;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.SystemPropsUtil;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.type.Type;
+
+import java.util.Properties;
+
+/**
+ * @author fengz (windywany@gmail.com)
+ * @version 1.0.0
+ * @since 1.0.0
+ **/
+@Slf4j
+public class SnowflakeIdGenerator implements IdentifierGenerator {
+
+    private Snowflake snowflake;
+
+    @Override
+    public void configure(Type type, Properties parameters, ServiceRegistry serviceRegistry) {
+        val localhostStr = NetUtil.getLocalhostStr();
+        val ipv4 = StringUtils.defaultIfBlank(localhostStr, NetUtil.LOCAL_IP);
+        long workerId = NetUtil.ipv4ToLong(ipv4) % 32;
+        long dc = SystemPropsUtil.getInt("snowflake.dc.id", 1) % 32;
+        log.debug("ipv4={}, Worker ID={}, DataCenter ID={}", ipv4, workerId, dc);
+        snowflake = IdUtil.getSnowflake(workerId, dc);
+    }
+
+    @Override
+    public Object generate(SharedSessionContractImplementor sharedSessionContractImplementor, Object o) {
+        return snowflake.nextId();
+    }
+
+}
