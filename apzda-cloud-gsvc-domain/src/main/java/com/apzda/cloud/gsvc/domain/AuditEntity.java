22e007035c35f6d2ec1375d61de04ed9c5d5e17a
ninggf <windywany@163.com>
2024-01-19 17:30:26 +0800

    add AuditEntity and TenantEntity

---
+++ apzda-cloud-gsvc-domain/src/main/java/com/apzda/cloud/gsvc/domain/AuditEntity.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2023-2024 Fengz Ning (windywany@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.apzda.cloud.gsvc.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * @author fengz (windywany@gmail.com)
+ * @version 1.0.0
+ * @since 1.0.0
+ **/
+@GenericGenerator(name = "snowflake", type = SnowflakeIdGenerator.class)
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AutoMetaListener.class)
+public abstract class AuditEntity implements AuditedEntity, SoftDeletedEntity {
+
+    public static final String SNOWFLAKE = "snowflake";
+
+    @Id
+    @GeneratedValue(generator = SNOWFLAKE, strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String createdBy;
+
+    private Long createdAt;
+
+    private String updatedBy;
+
+    private Long updatedAt;
+
+    private boolean deleted;
+
+}
