7461e9488653ba628ebe7a147f8225dbaa9225c2
ninggf <windywany@163.com>
2024-02-02 18:00:47 +0800

    upgrade brave-bom to 5.18.0

--- apzda-cloud-gsvc-core/src/main/java/com/apzda/cloud/gsvc/context/GsvcContextThreadLocalAccessor.java
+++ apzda-cloud-gsvc-core/src/main/java/com/apzda/cloud/gsvc/context/GsvcContextThreadLocalAccessor.java
@@ -2,7 +2,9 @@ package com.apzda.cloud.gsvc.context;
 
 import com.apzda.cloud.gsvc.core.GsvcContextHolder;
 import io.micrometer.context.ThreadLocalAccessor;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.lang.NonNull;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -10,6 +12,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 /**
  * @author fengz
  */
+@Slf4j
 public class GsvcContextThreadLocalAccessor implements ThreadLocalAccessor<RequestAttributes> {
 
     public static final String KEY = "gsvc.context";
@@ -22,19 +25,29 @@ public class GsvcContextThreadLocalAccessor implements ThreadLocalAccessor<Reque
 
     @Override
     public RequestAttributes getValue() {
+        if (log.isTraceEnabled()) {
+            log.trace("gsvc.context.getValue: {}", RequestContextHolder.getRequestAttributes());
+        }
         return RequestContextHolder.getRequestAttributes();
     }
 
     @Override
     public void setValue(@NonNull RequestAttributes value) {
-        RequestContextHolder.setRequestAttributes(value);
+        if (log.isTraceEnabled()) {
+            log.trace("gsvc.context.setValue: {}", value);
+        }
+        RequestContextHolder.setRequestAttributes(value, true);
         val requestId = GsvcContextHolder.getRequestId();
-        GsvcContextHolder.setRequestId(requestId);
+        if (StringUtils.isNotBlank(requestId)) {
+            GsvcContextHolder.setRequestId(requestId);
+        }
     }
 
     @Override
     public void setValue() {
+        log.trace("gsvc.context.setValue: null");
         RequestContextHolder.setRequestAttributes(null);
+        GsvcContextHolder.setRequestId(null);
     }
 
 }
