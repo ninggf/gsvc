3c0bef27472619b92ba9ac744922879c14c13aa3
ninggf <windywany@163.com>
2024-02-05 13:37:43 +0800

    inject MetaObjectHandler Bean only when  CurrentUserProvider bean is present

--- apzda-cloud-gsvc-starter/src/main/java/com/apzda/cloud/gsvc/autoconfigure/MyBatisPlusAutoConfiguration.java
+++ apzda-cloud-gsvc-starter/src/main/java/com/apzda/cloud/gsvc/autoconfigure/MyBatisPlusAutoConfiguration.java
@@ -40,6 +40,7 @@ import net.sf.jsqlparser.expression.StringValue;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -181,6 +182,7 @@ public class MyBatisPlusAutoConfiguration {
     }
 
     @Bean
+    @ConditionalOnBean(CurrentUserProvider.class)
     @ConditionalOnMissingBean
     MetaObjectHandler metaObjectHandler(CurrentUserProvider currentUserProvider,
             ObjectProvider<TenantManager> tenantManagers) {
