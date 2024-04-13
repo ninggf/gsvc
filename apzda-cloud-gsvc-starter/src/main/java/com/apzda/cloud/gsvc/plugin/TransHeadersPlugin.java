5c76771b9be3d58288da5d37535cd04cb9882399
ninggf <windywany@163.com>
2024-04-13 09:44:36 +0800

    去除多余的请求头

--- apzda-cloud-gsvc-starter/src/main/java/com/apzda/cloud/gsvc/plugin/TransHeadersPlugin.java
+++ apzda-cloud-gsvc-starter/src/main/java/com/apzda/cloud/gsvc/plugin/TransHeadersPlugin.java
@@ -50,6 +50,9 @@ public class TransHeadersPlugin implements IGlobalPlugin, IPreCall {
             return HttpHeadersFilter.filterRequest(headersFilters, new ServletServerHttpRequest(curRequest));
         }).orElse(new HttpHeaders());
 
+        headers.remove("Accept");
+        headers.remove("Content-Type");
+        headers.remove("Content-Length");
         headers.remove("X-Gsvc-Caller");
         headers.add("X-Gsvc-Caller", appName);
         headers.remove(HttpHeaders.HOST);
