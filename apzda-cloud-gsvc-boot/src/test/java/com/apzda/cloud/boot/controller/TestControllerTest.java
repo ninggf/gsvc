95605e27f6be0c6697ba4e015b11886b80b6d03c
ninggf <windywany@gmail.com>
2024-07-21 08:04:10 +0800

    补全测试用例

--- apzda-cloud-gsvc-boot/src/test/java/com/apzda/cloud/boot/controller/TestControllerTest.java
+++ apzda-cloud-gsvc-boot/src/test/java/com/apzda/cloud/boot/controller/TestControllerTest.java
@@ -18,6 +18,7 @@ package com.apzda.cloud.boot.controller;
 
 import com.apzda.cloud.boot.TestApp;
 import com.apzda.cloud.boot.autoconfig.GsvcBootAutoConfiguration;
+import com.apzda.cloud.boot.entity.User;
 import com.apzda.cloud.gsvc.security.config.GsvcSecurityAutoConfiguration;
 import com.apzda.cloud.test.autoconfig.AutoConfigureGsvcTest;
 import com.baomidou.mybatisplus.test.autoconfigure.AutoConfigureMybatisPlus;
@@ -26,6 +27,7 @@ import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
@@ -35,7 +37,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -46,12 +51,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  **/
 @WebMvcTest(TestController.class)
 @ContextConfiguration(classes = TestApp.class)
-@ImportAutoConfiguration({ GsvcBootAutoConfiguration.class, GsvcSecurityAutoConfiguration.class })
+@ImportAutoConfiguration({ GsvcBootAutoConfiguration.class, GsvcSecurityAutoConfiguration.class,
+        AopAutoConfiguration.class })
 @ComponentScan("com.apzda.cloud.boot.service")
 @AutoConfigureMockMvc
 @AutoConfigureMybatisPlus
 @AutoConfigureGsvcTest
 @Sql("classpath:/schema.sql")
+@WithMockUser(authorities = "*:user.*")
 public class TestControllerTest {
 
     @Autowired
@@ -61,13 +68,113 @@ public class TestControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    @WithMockUser(authorities = "r:user.*")
     void test() throws Exception {
         val content = mvc
             .perform(post("/user").contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .content("pageSize=10&pageNumber=0&pageSorts=uid|desc,createdAt|asc&id=gt 1"))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.data.records[0].id").value("2"));
+    }
+
+    @Test
+    void testGet() throws Exception {
+        mvc.perform(get("/user/1").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.id").value("1"))
+            .andExpect(jsonPath("$.data.type").value("1"))
+            .andExpect(jsonPath("$.data.roles").value("1"))
+            .andExpect(jsonPath("$.data.rolesText").value("r1"))
+            .andExpect(jsonPath("$.data.typeText").value("Test1"));
+    }
+
+    @Test
+    void testAdd() throws Exception {
+        // given
+        val user = new User();
+        user.setId("user001");
+        user.setName("Leo Ning");
+        user.setRoles("1");
+        user.setType("1");
+
+        mvc.perform(put("/user").contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(user)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.id").value("user001"))
+            .andExpect(jsonPath("$.data.type").value("1"))
+            .andExpect(jsonPath("$.data.roles").value("1"))
+            .andExpect(jsonPath("$.data.rolesText").value("r1"))
+            .andExpect(jsonPath("$.data.typeText").value("Test1"));
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+        // given
+        val user = new User();
+        user.setName("Leo Ning");
+
+        mvc.perform(patch("/user/1").contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(user)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.id").value("1"))
+            .andExpect(jsonPath("$.data.name").value("Leo Ning"))
+            .andExpect(jsonPath("$.data.type").value("1"))
+            .andExpect(jsonPath("$.data.roles").value("1"))
+            .andExpect(jsonPath("$.data.rolesText").value("r1"))
+            .andExpect(jsonPath("$.data.typeText").value("Test1"));
+    }
+
+    @Test
+    void testDelete() throws Exception {
+        assertThatThrownBy(() -> {
+            mvc.perform(delete("/user/100").accept(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound());
+        }).hasMessageContaining("errCode = 992");
+
+        mvc.perform(delete("/user/1").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.id").value("1"))
+            .andExpect(jsonPath("$.data.name").value("u1"))
+            .andExpect(jsonPath("$.data.type").value("1"))
+            .andExpect(jsonPath("$.data.roles").value("1"))
+            .andExpect(jsonPath("$.data.rolesText").value("r1"))
+            .andExpect(jsonPath("$.data.typeText").value("Test1"));
+
+        assertThatThrownBy(() -> {
+            mvc.perform(get("/user/1").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+        }).hasMessageContaining("errCode = 992");
+
+        mvc.perform(get("/user/2").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+        mvc.perform(get("/user/3").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+    }
+
+    @Test
+    void testDeleteBatch() throws Exception {
+
+        mvc.perform(delete("/user").accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(List.of("1", "2"))))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data[0].id").value("1"))
+            .andExpect(jsonPath("$.data[0].name").value("u1"))
+            .andExpect(jsonPath("$.data[0].type").value("1"))
+            .andExpect(jsonPath("$.data[0].roles").value("1"))
+            .andExpect(jsonPath("$.data[0].rolesText").value("r1"))
+            .andExpect(jsonPath("$.data[0].typeText").value("Test1"))
+            .andExpect(jsonPath("$.data[1].id").value("2"))
+            .andExpect(jsonPath("$.data[1].name").value("u2"))
+            .andExpect(jsonPath("$.data[1].roles").value("2"))
+            .andExpect(jsonPath("$.data[1].rolesText").value("r2"));
+
+        assertThatThrownBy(() -> {
+            mvc.perform(get("/user/1").accept(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound());
+        }).hasMessageContaining("errCode = 992");
+
+        assertThatThrownBy(() -> {
+            mvc.perform(get("/user/2").accept(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound());
+        }).hasMessageContaining("errCode = 992");
+
+        mvc.perform(get("/user/3").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
     }
 
 }
