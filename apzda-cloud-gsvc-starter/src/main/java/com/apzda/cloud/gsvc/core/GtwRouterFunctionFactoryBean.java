a2513941078fdb88f57f01f1715d313031737fc7
ninggf <windywany@gmail.com>
2025-05-13 18:25:47 +0800

    Remove swagger document

--- apzda-cloud-gsvc-starter/src/main/java/com/apzda/cloud/gsvc/core/GtwRouterFunctionFactoryBean.java
+++ apzda-cloud-gsvc-starter/src/main/java/com/apzda/cloud/gsvc/core/GtwRouterFunctionFactoryBean.java
@@ -1,18 +1,11 @@
 package com.apzda.cloud.gsvc.core;
 
 import com.apzda.cloud.gsvc.config.GatewayServiceConfigure;
-import com.apzda.cloud.gsvc.dto.MessageType;
-import com.apzda.cloud.gsvc.dto.Response;
-import com.apzda.cloud.gsvc.error.ServiceError;
 import com.apzda.cloud.gsvc.exception.GsvcExceptionHandler;
 import com.apzda.cloud.gsvc.gtw.ProxyExchangeHandler;
 import com.apzda.cloud.gsvc.gtw.Route;
 import com.apzda.cloud.gsvc.gtw.RouteRegistry;
 import com.apzda.cloud.gsvc.server.IServiceMethodHandler;
-import com.apzda.cloud.gsvc.swagger.ProtobufMsgHelper;
-import com.apzda.cloud.gsvc.utils.ResponseUtils;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import jakarta.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +13,6 @@ import lombok.val;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springdoc.core.fn.builders.operation.Builder;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Value;
@@ -42,7 +34,6 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 import static com.apzda.cloud.gsvc.gtw.ProxyExchangeHandler.parseUri;
-import static org.springdoc.core.utils.Constants.OPERATION_ATTRIBUTE;
 
 /**
  * @author fengz
@@ -159,19 +150,6 @@ public class GtwRouterFunctionFactoryBean
         };
 
         builder.route((request -> match(request, path, actions, consumes, excludes)), func);
-
-        val apiDocEnabled = applicationContext.getEnvironment()
-            .getProperty("springdoc.api-docs.enabled", Boolean.class, true);
-
-        if (apiDocEnabled) {
-            try {
-                val operationBuilder = createOperationBuilder(route, serviceMethod);
-                builder.withAttribute(OPERATION_ATTRIBUTE, operationBuilder);
-            }
-            catch (Exception e) {
-                webLog.warn("Cannot create swagger document for: {} - {}", path, e.getMessage());
-            }
-        }
     }
 
     private void setupForward(RouterFunctions.Builder builder, Route route, ServiceInfo serviceInfo) {
@@ -289,94 +267,6 @@ public class GtwRouterFunctionFactoryBean
             webLog.trace("{} matched '{}' with segments: {}", reqPath, path, segments);
         }
         return matched;
-    }
-
-    private Builder createOperationBuilder(Route route, ServiceMethod serviceMethod) throws JsonProcessingException {
-        val ro = Builder.operationBuilder();
-        // ro.beanClass(serviceMethod.getInterfaceName()).beanMethod(serviceMethod.getDmName());
-        ro.operationId(route.absPath());
-        ro.tag(serviceMethod.getServiceName());
-        val summary = route.getSummary();
-        if (StringUtils.isNotBlank(summary)) {
-            ro.summary(summary);
-        }
-
-        val desc = route.getDesc();
-        if (StringUtils.isNotBlank(desc)) {
-            ro.description(desc);
-        }
-
-        val tags = route.getTags();
-        if (tags != null && tags.length > 0) {
-            ro.tags(tags);
-        }
-
-        // request
-        ProtobufMsgHelper.create(serviceMethod.getRequestType());
-        val requestBodyBuilder = org.springdoc.core.fn.builders.requestbody.Builder.requestBodyBuilder();
-        requestBodyBuilder.required(true);
-        requestBodyBuilder.content(newBuilder("Request", serviceMethod.getRequestType()));
-        ro.requestBody(requestBodyBuilder);
-        // response
-        val responseBuilder = org.springdoc.core.fn.builders.apiresponse.Builder.responseBuilder();
-        responseBuilder.content(newBuilder("Response", serviceMethod.getReturnType()));
-        responseBuilder.responseCode("200");
-        responseBuilder.description("Success Response");
-        ro.response(responseBuilder);
-        val errResponseBuilder = org.springdoc.core.fn.builders.apiresponse.Builder.responseBuilder();
-        errResponseBuilder.content(newBuilder("Error", serviceMethod.getReturnType()));
-        errResponseBuilder.responseCode("!200");
-        errResponseBuilder.description("Error Response");
-        ro.response(errResponseBuilder);
-
-        if (route.meta().isLogin()) {
-            val parameter = applicationContext.getEnvironment()
-                .getProperty("apzda.cloud.security.token-name", String.class, "Authorization");
-            val bearer = applicationContext.getEnvironment()
-                .getProperty("apzda.cloud.security.bearer", String.class, "Bearer");
-            val parameterBuilder = org.springdoc.core.fn.builders.parameter.Builder.parameterBuilder();
-            parameterBuilder.in(ParameterIn.HEADER);
-            parameterBuilder.required(true);
-            parameterBuilder.allowEmptyValue(false);
-            parameterBuilder.name(parameter);
-            parameterBuilder.example(bearer + " 1234567890");
-            ro.parameter(parameterBuilder);
-
-            ro.security(org.springdoc.core.fn.builders.securityrequirement.Builder.securityRequirementBuilder()
-                .name("JWT"));
-        }
-        return ro;
-    }
-
-    private org.springdoc.core.fn.builders.content.Builder newBuilder(String title, Class<?> contentClass)
-            throws JsonProcessingException {
-        val contentBuilder = org.springdoc.core.fn.builders.content.Builder.contentBuilder();
-        contentBuilder.mediaType(MediaType.APPLICATION_JSON_VALUE);
-        val schemaBuilder = org.springdoc.core.fn.builders.schema.Builder.schemaBuilder();
-        schemaBuilder.type("object");
-        schemaBuilder.nullable(false);
-        schemaBuilder.required(true);
-        schemaBuilder.title(title);
-        schemaBuilder.description("Class: " + contentClass.getName());
-        val defaultValue = ProtobufMsgHelper.create(contentClass);
-        if (title.equals("Response")) {
-            schemaBuilder.defaultValue(ResponseUtils.OBJECT_MAPPER.writeValueAsString(Response.success(defaultValue)));
-        }
-        else if (title.equals("Error")) {
-            schemaBuilder.description("Error Response");
-
-            schemaBuilder.defaultValue(ResponseUtils.OBJECT_MAPPER
-                .writeValueAsString(Response.error(ServiceError.SERVICE_UNAVAILABLE).type(MessageType.NOTIFY)));
-
-            schemaBuilder.externalDocs(
-                    org.springdoc.core.fn.builders.externaldocumentation.Builder.externalDocumentationBuilder()
-                        .url("https://gsvc.apzda.com/manual/errors"));
-        }
-        else {
-            schemaBuilder.defaultValue(ResponseUtils.OBJECT_MAPPER.writeValueAsString(defaultValue));
-        }
-        contentBuilder.schema(schemaBuilder);
-        return contentBuilder;
     }
 
 }
